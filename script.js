d6f6e1d55cfac33f403d746916ce841b9ba1c76b
muratdrmz <mrtdrmz@windowslive.com>
2023-03-20 21:20:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,98 @@
+const modal=document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+const btnCloseModal=document.querySelector('.btn--close-modal');
+const btnOpenModal=document.querySelector('.btn--show-modal');
+const btnScrollTo=document.querySelector('.btn--scroll-to');
+const section1=document.querySelector('#section--1');
+const nav=document.querySelector('.nav');
+const tabs=document.querySelectorAll('.operations__tab');
+const tabsContainer=document.querySelector('.operations__tab-container');
+const tabsContent=document.querySelectorAll('.operations__content');
+
+const navlinks=document.querySelector('.nav__links');
+
+///////////////////////////////////////
+// Modal window
+
+const openModal=function(e){
+ e.preventDefault();
+ modal.classList.remove('hidden');
+ overlay.classList.remove('hidden');
+}
+
+const closeModal=function(){
+ modal.classList.add('hidden');
+ overlay.classList.add('hidden');
+}
+
+// close modal open modal
+
+btnOpenModal.addEventListener("click", openModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener('click',closeModal);
+document.addEventListener('keydown',(e)=>{
+ if(e.key==='Escape'&& !modal.classList.contains('hidden'))
+ {closeModal();} 
+})
+
+///////////////////////////////////////
+// Button scrolling
+
+btnScrollTo.addEventListener('click',(e)=>{
+
+// console.log(e.target.getBoundingClientRect());
+// const s1coords=section1.getBoundingClientRect();
+
+// console.log('Current scroll (X/Y)', window.scrollX,window.scrollY);
+// console.log(
+//   "height/width viewport",
+//   document.documentElement.clientHeight,
+//   document.documentElement.clientWidth
+// );
+// window.scrollTo(s1coords.left+window.scrollX,s1coords.top+window.scrollY);
+section1.scrollIntoView({behavior:'smooth'});
+})
+///////////////////////////////////////
+// Page navigation
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+navlinks.addEventListener('click',(e)=>{
+  e.preventDefault();
+  // Matching strategy
+  if(e.target.classList.contains('nav__link')){
+   const id=e.target.getAttribute('href')
+   document.querySelector(id).scrollIntoView({behavior:'smooth'})  
+  }
+});
+///////////////////////////////////////
+// Tabbed component
+
+tabsContainer.addEventListener('click',(e)=>{
+  const clicked = e.target.closest(".operations__tab");
+   // Guard clause
+  if (!clicked) return;
+  // Remove active classes
+  tabs.forEach((t) => {
+    t.classList.remove("operations__tab--active");
+  });
+  tabsContent.forEach((c) => {
+    c.classList.remove("operations__content--active");
+  });
+  // Activate tab
+  clicked.classList.add("operations__tab--active");
+  // Activate content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+})
+///////////////////////////////////////
+// Menu fade animation
+
